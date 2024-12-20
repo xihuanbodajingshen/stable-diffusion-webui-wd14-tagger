09f516b3f01c5b9eddd3b78b3bce64d2dd87a022
xihuanbodajingshen <117348582+xihuanbodajingshen@users.noreply.github.com>
2024-12-20 19:09:16 +0800

    Update utils.py

--- tagger/utils.py
+++ tagger/utils.py
@@ -67,7 +67,7 @@ def refresh_interrogators() -> List[str]:
         exist_ok=True
     )
 
-     for path in os.scandir(getattr(shared.cmd_opts, 'deepdanbooru_projects_path', default_ddp_path)):
+    for path in os.scandir(getattr(shared.cmd_opts, 'deepdanbooru_projects_path', default_ddp_path)):
         if not path.is_dir():
             continue
 
